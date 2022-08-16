da84a7aa661df42881f601e48ff70aad1391d31c
Marcelo_Bettini <marcelobettini@yahoo.com.ar>
2022-08-16 20:17:45 -0300

    working fine

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useGet } from './hooks/useGet';
+import { StyleSheet, Text, View, Image, ActivityIndicator, TouchableWithoutFeedback } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const randomize = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
+  }
+
+  const [randomChar, setRandomChar] = useState(randomize(1, 826))
+  const [data, isLoading, error] = useGet(randomChar)
+  const handlePress = () => {
+    console.log("Touched");
+    const num = randomize(1, 826)
+    setRandomChar(num)
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isLoading &&
+        <ActivityIndicator size="large" color="black" />}
+      {data && !isLoading &&
+        <View style={[styles.width, styles.spacer]}>
+          <Text style={[styles.name, styles.rounded, styles.width, styles.black]}>{data.name}</Text>
+          <Image style={[styles.image, styles.rounded]} source={{ uri: data.image }}
+            onLoadEnd={() => console.log("load ended...")}
+          ></Image>
+          <View style={[styles.spacer]}>
+            <TouchableWithoutFeedback
+              onPressIn={handlePress}
+            >
+              <Text style={[styles.name, styles.rounded, styles.width, styles.blue, styles.button]}>RANDOMIZE</Text>
+            </TouchableWithoutFeedback>
+          </View>
+        </View>
+
+
+
+
+
+      }
+
     </View>
   );
 }
@@ -13,8 +47,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fafa',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  name: {
+    fontSize: 25,
+    fontWeight: '700',
+    textTransform: 'uppercase',
+    marginBottom: 10,
+    textAlign: 'center',
+    padding: 10,
+  },
+  black: {
+    backgroundColor: 'black',
+    color: 'white',
+  },
+  blue: {
+    backgroundColor: '#483D8B',
+    color: 'white',
+  },
+  rounded: {
+    borderRadius: 15
+  },
+  width: {
+    width: 300,
+  },
+  button: {
+    shadowColor: "white",
+    shadowOffset: {
+      width: 10,
+      height: 5
+    },
+    shadowOpacity: .5,
+    shadowRadius: 3,
+    elevation: 5
+  },
+  spacer: {
+    marginTop: 20,
+  }
 });
---
+++ hooks/useGet.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+const base_url = "https://rickandmortyapi.com/api/character/"
+
+export const useGet = (endpoint) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getData = async (endpoint) => {
+    setLoading(true)
+    try {
+      const res = await fetch(base_url + endpoint);
+      const json = await res.json()
+      setData(json)
+    } catch (error) {
+      setError(true)
+    } finally {
+      setTimeout(()=> setLoading(false),1000)
+    }
+  }
+
+  useEffect(() => {
+    getData(endpoint)
+  }, [endpoint])
+
+  return [data, isLoading, error]
+}
+
+
